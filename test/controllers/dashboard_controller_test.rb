f581c14d7e4552f2a3fbdd5fbd473fdde52f6604
bhushanv <bhushanvidhani@gmail.com>
2020-10-26 00:27:57 +0530

    Added test cases

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get companies list" do
+    get "/"
+    assert_response :success
+    assert @response.body.to_s.include? "Dunder Mifflin"
+    assert @response.body.to_s.include? "Sabre"
+  end
+
+  # List of test cases
+
+  # Dashboard
+  # filter_by_company_role
+  # filter_by_user_role
+  # reset table functionality
+
+  # Company
+  # company index
+  # create company
+  # show company
+  # update company
+  # delete company
+
+  # User
+  # user index
+  # create user
+  # show user
+  # update user
+  # delete user
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get users list" do
+    get "/users"
+    assert_response :success
+    assert @response.body.to_s.include? "Jim Halpert"
+    assert @response.body.to_s.include? "Michael Scott"
+  end
 end

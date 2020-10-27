c1d32861b41e27e1fa87c973084361061ef5ff44
bhushanv <bhushanvidhani@gmail.com>
2020-10-27 19:21:01 +0530

    Updated company queries

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,8 +6,8 @@ class Company < ApplicationRecord
   validates_presence_of :name, :role
   validates_uniqueness_of :name
 
-  scope :all_companies, -> {joins(:users).distinct.select("companies.*, count(users.*) as users_count").group(:id).order(users_count: :desc, name: :asc).limit(10)}
-  scope :filter_by_company_role, ->(role) { joins(:users).where(role: role).distinct.select("companies.*, count(users.*) as users_count").group(:id).order(users_count: :desc, name: :asc).limit(10)}
+  scope :all_companies, -> {left_outer_joins(:users).distinct.select("companies.*, count(users.*) as users_count").group(:id).order(users_count: :desc, name: :asc).limit(10)}
+  scope :filter_by_company_role, ->(role) { left_outer_joins(:users).where(role: role).distinct.select("companies.*, count(users.*) as users_count").group(:id).order(users_count: :desc, name: :asc).limit(10)}
   scope :filter_by_user_role, ->(role) { joins(:users).where("users.role": role).distinct.select("companies.*, count(users.*) as users_count").group(:id).order(users_count: :desc, name: :asc).limit(10)}
   scope :company_roles_count, -> {group(:role).count}
 
